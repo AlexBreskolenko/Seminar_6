4177f5ab3f284329d42b9e482c755148f02c5acc
Alex <abreskolenko@yandex.ru>
2023-02-28 19:54:30 +0300

    Create work fibonachi

---
+++ fibonachi/Program.cs
@@ -0,0 +1,19 @@
+﻿//Числа фибоначи
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+//Выводим первые 10 чисел фибоначи
+for(int i = 1; i < 10; i++)
+{
+    Console.Write($"{Fibonacci(i)} ");
+}
